5a3ceecd6fe656bd6ccb171bcb28a02ea21ac372
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-09-04 07:51:08 +0530

    Created Model and Db context

---
+++ NZWalk/NZWalk.API/Models/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalk.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lattitude { get; set; }
+        public double Longitude { get; set; }
+        public long Population { get; set; }
+
+        //Navigation
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
---
+++ NZWalk/NZWalk.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalk.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid WalkDifficultyID { get; set; }
+
+        //Navigation
+        public Region Region { get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
